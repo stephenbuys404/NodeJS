10559ff004726a0f40151599e695f185dafc0780
Stephen Buys <stephenbuys404@gmail.com>
2022-09-21 17:07:27 +0200

    Upload Chapter2

---
+++ CHAPTER2/printMessages.js
@@ -0,0 +1,3 @@
+"use strict"
+const messageModule = require("./messages");
+messageModule.messages.forEach(m => console.log(m))
---
+++ CHAPTER2/start.cmd
@@ -0,0 +1,5 @@
+node hello.js
+node positiveMessages.js
+node printer.js
+node printMessages.js
+pause
