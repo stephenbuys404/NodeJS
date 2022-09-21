7ceb773fbc7100d3ab2b902d845d3ba5e1d355e2
Stephen Buys <stephenbuys404@gmail.com>
2022-09-21 17:17:20 +0200

    Upload Chapter13

---
+++ CHAPTER13/confetti_cuisine/controllers/homeController.js
@@ -0,0 +1,12 @@
+"use strict"
+exports.logRequestPaths = (req, res, next) => {
+  console.log(`request made to: ${req.url}`);
+  next();
+};
+exports.sendReqParam = (req, res) => {
+  let veg = req.params.vegetable;
+  res.send(`This is the page for ${veg}`);
+};
+exports.respondWithName = (req, res) => {
+  res.render("index");
+};
---
+++ CHAPTER13/confetti_cuisine/editdb.cmd
@@ -0,0 +1,5 @@
+mongo
+::type db to get placed in test database
+::to view all available databases type "show dbs"
+::"use <new_database_name>"create a new database and simultaneously switch into it
+::db.contacts.insert({name: "Jon Wexler",email: "jon@jonwexler.com",note: "Decent guy."})
